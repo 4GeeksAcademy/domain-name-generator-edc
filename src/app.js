e7bf1a4adfce7ef73be5a119bfc0dc02cdf5c307
emanueldelc <165728564+emanueldelc@users.noreply.github.com>
2024-06-05 21:45:20 +0000

    complete

--- src/app.js
+++ src/app.js
@@ -7,5 +7,23 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our", "he", "her", "it"];
+  let adj = ["great", "big", "minuscule", "important", "global"];
+  let noun = ["jogger", "racoon", "teacher", "geeks", "engineer"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let a = 0; a < adj.length; a++) {
+      for (let n = 0; n < noun.length; n++) {
+        let div = document.createElement(`div`);
+        div.innerText = `
+        ${pronoun[i]}${adj[a]}${noun[n]}.com
+        ${pronoun[i]}${adj[a]}${noun[n]}.org
+        ${pronoun[i]}${adj[a]}${noun[n]}.net
+        ${pronoun[i]}${adj[a]}${noun[n]}.edu
+        ${pronoun[i]}${adj[a]}${noun[n]}.io
+        `;
+        document.body.appendChild(div);
+      }
+    }
+  }
 };
